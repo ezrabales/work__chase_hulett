3334dfefb709dbfbd6c5ae598b322d1457d17752
Ezra Bales <bales.ezra@gmail.com>
2025-10-05 04:22:12 -0600

    fix domain name

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,7 @@
-const BASE_URL = "http://localhost:3001";
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://api.chase-hulett.crabdance.com"
+    : "http://localhost:3001";
 
 function checkResponse(res) {
   return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
